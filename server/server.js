e07ff8ffa2694ba10617193becba80c4333640b8
-089 <089rahulpatil@gmail.com>
2026-09-08 03:04:28 +0530

    fix: load server env explicitly at startup

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
+dotenv.config({ path: new URL('./.env', import.meta.url) });
+
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
